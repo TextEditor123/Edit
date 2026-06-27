527c743dc5be18cd29cbd364dc258796a25ae800
Luthetus <luthetus@gmail.com>
2026-06-27 10:46:17 -0400

    referring to these Uint*Array members by name through

--- src/RendererFiles/fieldBuffer.js
+++ src/RendererFiles/fieldBuffer.js
@@ -24,6 +24,9 @@ So, by allocating a Uint*Array, I can create a single reference that the garbage
 It sees that the children of that Uint*Array are primitive values, and thus it doesn't have to visit the children.
 Thus 64 number variables, that would've been 64 visits during the marking phase of GC, become just 1 visit.
 
+
+The next thing I'm doing is referring to these Uint*Array members by name through the use of const fat arrow functions.
+I want to avoid the cost of invoking these const fat arrow functions, and remove the cost of their definitions though.
 The first statement needs to be that a JS engine might actually do what this file does at runtime through
 their own inlining, or caching. But I wanted to ensure it occured in a way that felt confidently in control of.
 
